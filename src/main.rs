871f0391c1e790c4a7f5a718799c605f29f28dc3
Graeme Read <graeme@sigma957.net>
2025-11-07 21:58:24 +0000

    Implement a REPL (#FF0)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => println!("{}: command not found", input.trim_end()),
-        Err(error) => eprintln!("error: {error}"),
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => println!("{}: command not found", input.trim_end()),
+            Err(error) => eprintln!("error: {error}"),
+        }
     }
 }
