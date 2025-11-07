6e6ffafc4c44e1476af6c7aabdb3c8879cb181d5
Graeme Read <graeme@sigma957.net>
2025-11-07 22:22:24 +0000

    Implement exit (#PN5)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::{self, Write};
 
+const BUILTINS: [&str; 1] = ["exit"];
+
 fn main() {
     loop {
         print!("$ ");
@@ -7,8 +9,31 @@ fn main() {
 
         let mut input = String::new();
         match io::stdin().read_line(&mut input) {
-            Ok(_) => println!("{}: command not found", input.trim_end()),
+            Ok(_) => {
+                let line = input.trim();
+                let mut tokens = line.split_whitespace();
+                let Some(command) = tokens.next() else {
+                    continue;
+                };
+
+                if BUILTINS.contains(&command) {
+                    if command == "exit" {
+                        let status = tokens
+                            .next()
+                            .map_or(0, |token| token.parse::<i32>().unwrap_or_default());
+
+                        exit(status);
+                    }
+                } else {
+                    println!("{line}: command not found");
+                }
+            }
             Err(error) => eprintln!("error: {error}"),
         }
     }
+}
+
+// TODO: Move to builtin module
+fn exit(status: i32) {
+    std::process::exit(status);
 }
